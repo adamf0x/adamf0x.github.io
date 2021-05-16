12d986d682d2d9baedddb5de68a12a7433c1cd87
adamf0x <adamfox96@gmail.com>
2021-05-15 18:46:05 -0700

    added fade in animation for sections

--- js/scripts.js
+++ js/scripts.js
@@ -67,3 +67,39 @@
   
   })(jQuery); // End of use strict
   
+  let scrollElements = document.querySelectorAll(".js-scroll");
+
+  scrollElements.forEach(element=>{
+    element.style.opacity=0
+  })
+
+  const elementInView = (el, scrollOffset = 50) => {
+    const elementTop = el.getBoundingClientRect().top;
+   
+    return (
+      elementTop <= 
+      ((window.innerHeight || document.documentElement.clientHeight) - scrollOffset)
+    );
+  };
+
+  const displayScrollElement = element=>{
+    element.classList.add("scrolled");
+  }
+
+  const hideScrollElement = (element) => {
+    element.classList.remove("scrolled");
+  }
+
+  const handleScrollAnimation = () => {
+    scrollElements.forEach((el) => {
+      if (elementInView(el, 100)) {
+        displayScrollElement(el);
+      } else {
+        hideScrollElement(el);
+      }
+    })
+  }
+
+  window.addEventListener('scroll', () => {
+    handleScrollAnimation();
+  })
